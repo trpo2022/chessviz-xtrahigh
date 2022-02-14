f185420aa5c27cb7c49dc3cdbcf0bab0e4a1fd94
xtrahigh <52545668+xtrahigh@users.noreply.github.com>
2022-02-15 01:11:21 +0700

    Implement board output to stdout

--- src/chessviz.c
+++ src/chessviz.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#define SIZE 8
+
+void print_board(int size, char array[][size + 1])
+{
+    for (int i = SIZE - 1; i >= 0; i--) {
+        // Row number
+        printf("%d | ", i + 1);
+
+        // Board rows
+        for (int j = 0; j < SIZE; j++) {
+            printf("%c ", array[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Border
+    printf("    - - - - - - - -\n");
+
+    // Column chars
+    printf("    a b c d e f g h\n");
+}
 
 int main(void)
 {
-    printf("Hello World!\n");
+    char board[SIZE][SIZE + 1]
+            = {"RNBQKBNR", "PPPPPPPP", "", "", "", "", "pppppppp", "rnbqkbnrp"};
+
+    print_board(SIZE, board);
+
+    return 0;
 }
