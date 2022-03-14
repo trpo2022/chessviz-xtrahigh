c4d7f71f7f66c657af999bf2517b53dab35394f5
xtrahigh <52545668+xtrahigh@users.noreply.github.com>
2022-03-14 20:34:57 +0700

    Split main source file into separate functions

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include "print_board_plain.h"
+
+int main(void)
+{
+    char board[BOARD_SIZE][BOARD_SIZE + 1]
+            = {"RNBQKBNR", "PPPPPPPP", "", "", "", "", "pppppppp", "rnbqkbnrp"};
+
+    print_board(board);
+
+    return 0;
+}
---
+++ src/print_board_plain.h
@@ -0,0 +1,8 @@
+#ifndef PRINT_BOARD_H
+#define PRINT_BOARD_H
+
+#define BOARD_SIZE 8
+
+void print_board(char array[][BOARD_SIZE + 1]);
+
+#endif
