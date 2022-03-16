4c74aac220bb1afaf9d6cc95e1ab651d42fb114c
xtrahigh <52545668+xtrahigh@users.noreply.github.com>
2022-03-16 23:01:22 +0700

    Rework main chessviz code in accordance with new functions

--- src/chessviz/main.c
+++ src/chessviz/main.c
@@ -1,11 +1,13 @@
-#include <libchessviz/print_board_plain.h>
+#include <libchessviz/board.h>
 
 int main(void)
 {
-    char board[BOARD_SIZE][BOARD_SIZE + 1]
-            = {"RNBQKBNR", "PPPPPPPP", "", "", "", "", "pppppppp", "rnbqkbnrp"};
+    ChessBoard* b1 = create_board();
 
-    print_board(board);
+    print_board(b1, stdout);
+
+    if (read_moves(b1, stdin))
+        make_moves(b1);
 
     return 0;
 }
