ae960ba5b94b7fe55adb84301118ee8257730a6b
c1ta <cita@cthd.icu>
2022-03-23 18:55:42 +0700

    Implement a function to free allocated memory for board struct

--- src/chessviz/main.c
+++ src/chessviz/main.c
@@ -1,5 +1,6 @@
 #include <libchessviz/chess_board.h>
 #include <libchessviz/create_board.h>
+#include <libchessviz/free_board.h>
 #include <libchessviz/move.h>
 #include <libchessviz/print_board.h>
 #include <libchessviz/read_board.h>
@@ -12,6 +13,8 @@ int main(void)
 
     if (read_moves(b1, stdin))
         make_moves(b1);
+
+    free_board(b1);
 
     return 0;
 }
