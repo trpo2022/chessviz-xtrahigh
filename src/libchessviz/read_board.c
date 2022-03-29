28a941750a30c96238dafdac037e011eb352079a
xtrahigh <52545668+xtrahigh@users.noreply.github.com>
2022-03-30 01:40:00 +0700

    Split turn input in read_moves() function
    Use case: last turn
    If a checkmate was made by the first move in last turn, the input stops.

--- src/libchessviz/read_board.c
+++ src/libchessviz/read_board.c
@@ -58,14 +58,16 @@ static int is_correct_move(char* move)
 
 int read_moves(ChessBoard* b, FILE* src)
 {
-    int turn = -1;
+    int turn = -1, is_end = 1;
     char white_move[MOVE_SIZE] = {'\0'};
     char black_move[MOVE_SIZE] = {'\0'};
 
     fprintf(stdout, "\nEnter the full record of turns (CTRL+C to quit)\n");
     fprintf(stdout, "Example:\n1. e2-e4 e7-e5\n2. f2-f3 b7-b6\n3. d2-d3#\n\n");
 
-    while (fscanf(src, "%d. %s %s", &turn, white_move, black_move) != EOF) {
+    while (is_end != EOF) {
+		is_end = fscanf(src, "%d. %s", &turn, white_move);
+
         if (turn < 1 || turn != b->turns + 1) {
             fprintf(stderr,
                     "Turn %d: incorrect number, expected %d\n",
@@ -88,6 +90,8 @@ int read_moves(ChessBoard* b, FILE* src)
 
         if (is_last_move(white_move))
             return 1;
+
+        is_end = fscanf(src, "%s", black_move);
 
         if (!is_correct_move(black_move)) {
             fprintf(stderr,
