eb54a1622549963285d15ca5f5bd4b63cf2826b0
xtrahigh <52545668+xtrahigh@users.noreply.github.com>
2022-05-12 01:07:14 +0700

    Add pawn move correctness tests

--- test/move.c
+++ test/move.c
@@ -5,7 +5,7 @@
 
 #include <ctest.h>
 
-CTEST(movements, move_piece)
+CTEST(move, move_piece)
 {
     ChessBoard* board = create_board();
     ASSERT_NOT_NULL(board);
@@ -52,6 +52,41 @@ CTEST(movements, move_piece)
 
     result = move_piece(board, fc, fr, tc, tr);
     ASSERT_FALSE(result);
+
+    free_board(board);
+}
+
+CTEST(move, is_pawn_move_correct)
+{
+    ChessBoard* board = create_board();
+    ASSERT_NOT_NULL(board);
+
+    ASSERT_TRUE(is_pawn_move_correct(board, "a2-a4"));
+    ASSERT_TRUE(is_pawn_move_correct(board, "b2-b4"));
+    ASSERT_TRUE(is_pawn_move_correct(board, "c2-c4"));
+    ASSERT_TRUE(is_pawn_move_correct(board, "d2-d4"));
+    ASSERT_TRUE(is_pawn_move_correct(board, "e2-e4"));
+    ASSERT_TRUE(is_pawn_move_correct(board, "f2-f4"));
+    ASSERT_TRUE(is_pawn_move_correct(board, "g2-g4"));
+
+    ASSERT_TRUE(is_pawn_move_correct(board, "a2-a3"));
+    ASSERT_TRUE(is_pawn_move_correct(board, "b2-b3"));
+    ASSERT_TRUE(is_pawn_move_correct(board, "c2-c3"));
+    ASSERT_TRUE(is_pawn_move_correct(board, "d2-d3"));
+    ASSERT_TRUE(is_pawn_move_correct(board, "e2-e3"));
+    ASSERT_TRUE(is_pawn_move_correct(board, "f2-f3"));
+    ASSERT_TRUE(is_pawn_move_correct(board, "g2-g3"));
+
+    ASSERT_FALSE(is_pawn_move_correct(board, "a2-a5"));
+    ASSERT_FALSE(is_pawn_move_correct(board, "b2-b5"));
+    ASSERT_FALSE(is_pawn_move_correct(board, "c2-c5"));
+    ASSERT_FALSE(is_pawn_move_correct(board, "d2-d5"));
+    ASSERT_FALSE(is_pawn_move_correct(board, "e2-e5"));
+    ASSERT_FALSE(is_pawn_move_correct(board, "f2-f5"));
+    ASSERT_FALSE(is_pawn_move_correct(board, "g2-g5"));
+    ASSERT_FALSE(is_pawn_move_correct(board, "g2-d2"));
+    ASSERT_FALSE(is_pawn_move_correct(board, "g2-d5"));
+    ASSERT_FALSE(is_pawn_move_correct(board, "g2-c1"));
 
     free_board(board);
 }
