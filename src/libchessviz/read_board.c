4300316a31bdb4cb6c3377160268d6e688a15edc
xtrahigh <52545668+xtrahigh@users.noreply.github.com>
2022-05-12 02:45:19 +0700

    Change sample read entry to correct
    From https://csc-software-development.readthedocs.io/ru/2022/assignment.html

--- src/libchessviz/read_board.c
+++ src/libchessviz/read_board.c
@@ -71,7 +71,9 @@ int read_moves(ChessBoard* b, FILE* src)
     char black_move[MOVE_SIZE] = {'\0'};
 
     fprintf(stdout, "\nEnter the full record of turns (CTRL+C to quit)\n");
-    fprintf(stdout, "Example:\n1. e2-e4 e7-e5\n2. f2-f3 b7-b6\n3. d2-d3#\n\n");
+    fprintf(stdout,
+            "Example:\n1. e2-e4 e7-e5\n2. Bf1-c4 Nb8-c6\n3. Qd1-h5 Ng8-f6\n4. "
+            "Qh5xf7#\n\n");
 
     while (is_end != EOF) {
         is_end = fscanf(src, "%d. %s", &turn, white_move);
